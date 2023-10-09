f0ef0ca5c2a3f61cf9f778af9853719eb8128c19
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-09 16:42:09 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+
+
+def greet():
+    print('Hello World')
+
+# name = input("Input a noun: ")
+# action = input("Input a verb in the past tense: ")
+# print (f"Hello, {name}, I heard you {action}")
